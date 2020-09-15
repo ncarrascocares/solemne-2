2e3ecef9607add4f023ef7582c859387ae4f9e1e
nicolascarrascocares <53540839+nicolascarrascocares@users.noreply.github.com>
2020-09-15 15:44:45 -0400

    tercer commit, se crean las clase hijas Escritorio y Notebook

--- Carrasco_Nicolas/src/negocio/Computador.java
+++ Carrasco_Nicolas/src/negocio/Computador.java
@@ -7,4 +7,70 @@ package negocio;
  */
 public class Computador {
     
+      //Atributos
+    private String sistemaOperativo;
+    private String marca;
+    private String modelo;
+    private int peso;
+    static final int RAM = 4;
+    
+    //Constructor sin parametros
+    public Computador(){
+        
+    }
+    
+     //Constructor con parametros
+    public Computador(String sistemaOperativo, String marca, String modelo, int peso) {
+        this.sistemaOperativo = sistemaOperativo;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.peso = peso;
+        //this.ram = 4;
+        
+    }
+
+    public String getSistemaOperativo() {
+        return sistemaOperativo;
+    }
+
+    public void setSistemaOperativo(String sistemaOperativo) {
+        this.sistemaOperativo = sistemaOperativo;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public int getPeso() {
+        return peso;
+    }
+
+    public void setPeso(int peso) {
+        this.peso = peso;
+    }
+
+    public int getRam() {
+        return RAM;
+    }
+
+    //Metodo de la clase Padre
+    public void mostrarDatos(){
+        System.out.println("Sistema Operativo : " + sistemaOperativo);
+        System.out.println("Marca             : " + marca);
+        System.out.println("Modelo            : " + modelo);
+        System.out.println("Peso              : " + peso + " kilos");
+        System.out.println("Ram original      : " + RAM);
+    }
 }
