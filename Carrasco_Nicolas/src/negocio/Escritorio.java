24c20e6b20e264ec402296e3d4e8c137d690a5ef
nicolascarrascocares <53540839+nicolascarrascocares@users.noreply.github.com>
2020-09-15 15:24:36 -0400

    primer commit, creacion de proyecto solemne 2 USS

---
+++ Carrasco_Nicolas/src/negocio/Escritorio.java
@@ -0,0 +1,10 @@
+
+package negocio;
+
+/**
+ *
+ * @author ncarrasco
+ */
+public class Escritorio {
+    
+}
---
+++ Carrasco_Nicolas/src/negocio/Notebook.java
@@ -0,0 +1,10 @@
+
+package negocio;
+
+/**
+ *
+ * @author ncarrasco
+ */
+public class Notebook {
+    
+}
