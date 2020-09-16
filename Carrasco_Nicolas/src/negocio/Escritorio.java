9e97b57f3f44fdf703a89b8b4bf731997f23e8ba
nicolascarrascocares <53540839+nicolascarrascocares@users.noreply.github.com>
2020-09-15 22:37:40 -0400

    codificación completa del proyecto, donde se realiza el modelo mvc, trabajo para solemne 2 USS

--- Carrasco_Nicolas/src/negocio/Escritorio.java
+++ Carrasco_Nicolas/src/negocio/Escritorio.java
@@ -5,6 +5,34 @@ package negocio;
  *
  * @author ncarrasco
  */
-public class Escritorio {
+public class Escritorio extends Computador{
+    
+     //Atributo de la clase
+    private int ramAdicional;
+    
+    public Escritorio(){
+        
+    }
+    
+    public Escritorio(int ramAdicional, String sistemaOperativo, String marca, String modelo, int peso, int id) {
+        super(sistemaOperativo, marca, modelo, peso, id);
+        this.ramAdicional = ramAdicional;
+    }
+
+    public int getRamAdicional() {
+        return ramAdicional;
+    }
+
+    public void setRamAdicional(int ramAdicional) {
+        this.ramAdicional = ramAdicional;
+    }
+    
+    //Metodo para mostar informacion por pantalla
+    @Override
+     public void mostrarDatos(){
+        System.out.println("\nPC");
+        super.mostrarDatos();
+        System.out.println("Ram adicional     : " + getRamAdicional());
+    }
     
 }
